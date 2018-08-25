30aa55206ee90ff89092e4b07374e32bde88326c
FenixAlive <luisangelmf@gmail.com>
2018-08-25 17:45:13 -0500

    agregue mas decimales

--- cambioDobleBase.m
+++ cambioDobleBase.m
@@ -9,10 +9,9 @@ clc
 
 cont = 1;
 
-format long;
+format long e;
 
 while cont
-
   
   numero = input("\n\tDime tu numero: ");
 
@@ -20,35 +19,48 @@ while cont
 
   baseDes = input("\n\tDime a que base quieres convertir el numero (mayor a 1): ");
 
-  %convertir el numero a array y separar el entero del punto
+  %separar parte entera de la fraccion
 
   entero = floor(numero);
+  format long e;
   decimal = numero - entero;
 
-  entero = num2str(entero);
-  decimal = num2str(decimal);
-  decimal = decimal(3:end);
+  %ver si la base del numero es decimal
+  if baseOrig == 10
 
-
-  % hacer ciclos para hacer cambio de base
-
-  %convertir entero a base 10
-  n = length(entero)-1;
-  entConv = 0;
+    entConv = entero;
+    decConv = decimal;
   
-  for i = entero
-    entConv = entConv + str2num(i)*baseOrig^n;
-    n--;
-  end
+  else
 
-  %convertir decimal a base 10
-  n = -1;
-  decConv = 0;
+    %convertir el numero a array y separar el entero del punto
+    
+    entero = num2str(entero);
+    decimal = num2str(decimal, 20);
+    decimal = decimal(3:end);
 
-  for i = decimal
-    decConv = decConv + str2num(i)*baseOrig^n;
-    n--;
-  end
+
+    % hacer ciclos para hacer cambio de base
+
+    %convertir entero a base 10
+    n = length(entero)-1;
+    entConv = 0;
+  
+    for i = entero
+      entConv = entConv + str2num(i)*baseOrig^n;
+      n--;
+    end
+
+    %convertir decimal a base 10
+    n = -1;
+    decConv = 0;
+
+    for i = decimal
+      decConv = decConv + str2num(i)*baseOrig^n;
+      n--;
+    end
+
+  endif
   
   %convertir de base 10 a la base deseada
 
@@ -70,7 +82,7 @@ while cont
   if (decConv != 0)
     fraNum = []; %aqui guarda la parte fraccionada del nuevo numero
 
-    for i=0:9 %tendra 10 ceros despues del punto
+    for i=0:19 %tendra 20 ceros despues del punto
 
       % agrega un nuevo elemento a la derecha del vector, que es el entero de multiplicar la fraccion del numero por la base
       decConv = decConv*baseDes;
